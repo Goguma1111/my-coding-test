6754b6719c09622a8641ceb821f9c47e94489134
goguma <doffltm1125@naver.com>
2025-02-27 12:23:00 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.py
@@ -0,0 +1,19 @@
+def solution(dot):
+    answer = 0
+
+    if dot[0] > 0 and dot[1] > 0 : 
+        answer = 1
+
+    elif dot[0] < 0 and dot[1] > 0 :
+        answer = 2
+
+    elif dot[0] < 0 and dot[1] < 0 :
+        answer = 3
+
+    elif dot[0] > 0 and dot[1] < 0 :
+        answer = 4          
+
+    return answer
+
+print(solution([1,1]))
+print(solution([-1, 3]))
