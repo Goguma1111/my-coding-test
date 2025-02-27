4ddd4c031afb8102136325e9a025c50b32defccc
goguma <doffltm1125@naver.com>
2025-02-27 12:55:38 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.py
@@ -0,0 +1,11 @@
+def solution(numbers, direction):
+    answer = []
+    if direction == "right" :
+        answer = numbers[-1:] + numbers[:-1]
+    elif direction == "left" :
+        answer = numbers[1:] + numbers[:1]
+
+    return answer
+
+print(solution([1,2,3], "right"))
+print(solution([4,455,6,4,-1,45,6] ,"left"))
