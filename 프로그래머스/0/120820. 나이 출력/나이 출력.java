e9155b7d130bc133079cb39fc7c614b9cf8da949
goguma <doffltm1125@naver.com>
2025-03-14 13:35:46 +0900

    [level 0] Title: 나이 출력, Time: 0.02 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.java
@@ -0,0 +1,10 @@
+class Solution {
+    public static int solution(int age) {
+        int answer = 2022 - age + 1;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(23));
+    }
+}
