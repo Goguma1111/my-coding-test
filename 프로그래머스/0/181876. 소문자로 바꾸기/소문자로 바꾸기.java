d78e3f821379c8aa2575a55237e5654edf571e1d
goguma <doffltm1125@naver.com>
2025-03-17 15:23:50 +0900

    [level 0] Title: 소문자로 바꾸기, Time: 0.04 ms, Memory: 87.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181876. 소문자로 바꾸기/소문자로 바꾸기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public static String solution(String myString) {
+        String answer = myString.toLowerCase();
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("aBcDefG"));
+    }
+}
