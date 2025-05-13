ec9560d70b7df578cb9cda64050080a44dbb592a
goguma <doffltm1125@naver.com>
2025-05-13 16:44:38 +0900

    [level 0] Title: 각도기, Time: 0.02 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.js
@@ -0,0 +1,16 @@
+function solution(angle) { // 각도기
+    let answer = 0;
+
+    if (0 < angle && angle < 90) {
+        return answer = 1;
+    } else if (angle == 90) {
+        return answer = 2;
+    } else if (90 < angle && angle < 180) {
+        return answer = 3;
+    } else  {
+        return answer = 4;
+    }
+}
+
+
+console.log(solution(70));
