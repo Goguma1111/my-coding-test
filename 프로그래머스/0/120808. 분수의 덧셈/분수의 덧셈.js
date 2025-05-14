6eb0e93191add1bf9f7767a4560ebd4d848c8470
goguma <doffltm1125@naver.com>
2025-05-14 12:41:30 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.10 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,14 +1,4 @@
-function solution(numer1, denom1, numer2, denom2) { // 분수의 덧셈
-    let answer = []
-    let numer = (numer1 * denom2) + (numer2 * denom1);
-    let denom =  denom1 * denom2;
-    const gcd = (numer, denom) => (numer % denom === 0 ? denom : gcd(denom, numer % denom))
-
-    answer[0] = numer / gcd(numer, denom);
-    answer[1] = denom / gcd(numer, denom);
-
+function solution(numer1, denom1, numer2, denom2) {
+    var answer = [];
     return answer;
 }
-
-console.log(solution(1,2,3,4));
-console.log(solution(9,2,1,3));
